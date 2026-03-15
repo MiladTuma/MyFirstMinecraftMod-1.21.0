5a6c7abdc539fbc720c7998d2707fa06cbde4d23
Milad <Miladtuma1@gmail.com>
2026-03-15 18:05:34 +0100

    init commit

---
+++ src/main/java/com/milad/myfirstmod/myFirstModClient.java
@@ -0,0 +1,10 @@
+package com.milad.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class myFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
